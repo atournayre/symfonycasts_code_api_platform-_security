334a8e64df5528344b1ad832df35ac9560772f07
atournayre <aurelien.tournayre@gmail.com>
2021-07-14 20:55:11 +0200

    13. Logging in Inside the Test

--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -4,6 +4,7 @@
 namespace App\Tests\Functional;
 
 use App\ApiPlatform\Test\ApiTestCase;
+use App\Entity\User;
 
 class CheeseListingResourceTest extends ApiTestCase
 {
@@ -19,6 +20,28 @@ class CheeseListingResourceTest extends ApiTestCase
         ]);
 
         $this->assertResponseStatusCodeSame(401);
+
+        $user = new User();
+        $user->setEmail('cheese_tester@example.com');
+        $user->setUsername('cheese_tester');
+        $user->setPassword('$argon2id$v=19$m=65536,t=6,p=1$HfozEktpWfdQmoXZz9sCYA$72/pDN2ZA1591NsBDdRMuUbziZVKzwx+/zaCywyKmNQ');
+
+        $em = self::$container->get('doctrine')->getManager();
+
+        $em->persist($user);
+        $em->flush();
+
+        $client->request('POST', '/login', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'email' => 'cheese_tester@example.com',
+                'password' => 'foo',
+            ],
+        ]);
+
+        $this->assertResponseStatusCodeSame(204);
 
     }
 }
