d9bb3248aa66a54d7358e4687eac6f25561afe73
atournayre <aurelien.tournayre@gmail.com>
2021-07-14 17:04:56 +0200

    07. Logout & Passing API Data to JS on Page Load

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -26,4 +26,12 @@ class SecurityController extends AbstractController
             'Location' => $iriConverter->getIriFromItem($this->getUser()),
         ]);
     }
+
+    /**
+     * @Route("logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('Should not be reached');
+    }
 }
